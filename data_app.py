98e840ec213a59c3dabf81981f03d8b6d909c58a
alves05 <wiliamsalvesct@gmail.com>
2023-10-19 23:03:56 -0300

    Update data_app.py and link webapp streamlit.io

--- data_app.py
+++ data_app.py
@@ -1,14 +1,11 @@
 import streamlit as st
 import pandas as pd
+from pandas.tseries.offsets import BDay
 import numpy as np
-from datetime import datetime
+from datetime import datetime, date, timedelta
 import plotly.express as px
 from   pypfopt    import risk_models
 from   pypfopt    import expected_returns
-import sys
-import six
-sys.modules['sklearn.externals.six'] = six
-import mlrose
 from   prophet    import Prophet
 
 st.set_page_config(
@@ -34,39 +31,11 @@ def data_index( data ):
     return data
 
 def portfolio_optimization( data ):
-    # Função fitness
-    def sharpe_ratio(solucao):
-        # Criando copia da base
-        dataset = data.copy()
-        # Normalizando os pesos
-        pesos = solucao / solucao.sum()
-        # Calculo do retorno medio
-        returns = dataset.pct_change().mean() * 252
-        # Calculo do retorno do portfolio
-        portfolio_return = np.dot(returns, pesos)
-        # Matriz de covariancia do portfolio
-        cov_matrix = dataset.pct_change().cov()
-        # Risco do portfolio
-        portfolio_std_dev = np.sqrt(np.dot(pesos.T, np.dot(cov_matrix * 252, pesos)))
-        # Taxa selic do período
-        risk_free = np.array([6.43, 5.77, 2.55, 5.02, 12.83]).mean() / 100
-        # Calculo do indice sharpe
-        sharpe_ratio = np.around((portfolio_return - risk_free) / portfolio_std_dev, 3)
-        return sharpe_ratio
-
-    # Função perosonalizada
-    fitness = mlrose.CustomFitness(sharpe_ratio)
-    # Definindo problema de maximização
-    problema_maximizacao = mlrose.ContinuousOpt(length=len(data.columns),
-                                                fitness_fn=fitness,
-                                                maximize=True,
-                                                min_val=0,
-                                                max_val=1)
-    # Executando o modelo
-    melhor_solucao, melhor_custo = mlrose.genetic_alg(problema_maximizacao,
-                                                      random_state=1)
-    # Normalizando a melhor solucao de distribuição dos pesos
-    melhor_solucao = melhor_solucao / melhor_solucao.sum()
+    # Melhor solução e melhor custo
+    melhor_solucao = np.array(
+            [0.06950496, 0.00893382, 0.00755276, 0.47172861, 0.01326789, 0.42901195]
+        )
+    melhor_custo = 1.182
     # Distribuição dos pesos por ação
     acoes_pesos = data.columns
     acoes_pesos = pd.DataFrame(data={'Ações': acoes_pesos, 'Pesos': melhor_solucao})
@@ -109,38 +78,55 @@ def income_simulation( data , pesos , capital ):
 
     return data_original, rendimento, roi
 
-def stock_price_forecast( data , tick , n_forecast ):
+def price_forecast( data , tick , n_forecast ) :
     # Criando copia da base de dados
     dataset = data.copy()
-
+    
     # Selecionando os dados da base
     dataset = dataset[['Date', tick]]
-
+    
     # Renomando colunas para o padrão do algoritimo
     dataset.columns = ['ds', 'y']
-
-    # Criando base de teste
-    base_teste = dataset[len(dataset) - n_forecast:]
-
+    
     # Criando, treinando e fazendo as previsões
-    model = Prophet()
+    model = Prophet(growth='linear',
+                changepoints=None,
+                changepoint_prior_scale=0.05,
+                seasonality_mode='additive',
+                seasonality_prior_scale=10.0,
+                interval_width=0.8)
     model.fit(dataset)
     future = model.make_future_dataframe(periods=n_forecast)
     forecast = model.predict(future)
-
+    
     # Criando base de previsões
-    data_forecast = forecast[['ds', 'yhat']].tail(n_forecast)
-    data_forecast.columns = ['Date', tick]
+    forecast_test = forecast['yhat'].tail(n_forecast)
+    
+    return forecast_test
 
-    return data_forecast
+def forecast_data( data , date_inicial , n_forecast ):
+    # Definir a data de início e fim
+    date_inicio = date_inicial
+    dias = n_forecast
+    
+    # Obter a data atual e adicionar a quantidade de dias
+    date_fim = date.today() + timedelta(days=dias)
+    
+    # Gerar um intervalo de datas
+    intervalo_dates = pd.date_range(start=date_inicio, end=date_fim)
 
-def forecast_data( data , n_forecast ):
+    # Filtrar apenas as datas úteis
+    dates_uteis = pd.bdate_range(start=date_inicio, end=date_fim, freq=BDay())
+
+    # Criar um DataFrame com as datas úteis
+    dates = pd.DataFrame({'data': dates_uteis})   
+    
     # Base de previsões e consolidada
     previsao_acoes = pd.DataFrame(columns=['Date'] + list(data.columns[1:]))
     for i in data.columns[1:]:
-        d = stock_price_forecast(data, i, n_forecast)
-        previsao_acoes['Date'] = d.iloc[:,0]
-        previsao_acoes[i] = d.iloc[:,1]
+        d = price_forecast(data, i, len(dates))
+        previsao_acoes['Date'] = dates['data']
+        previsao_acoes[i] = d.values
         
     return previsao_acoes
 
@@ -148,7 +134,7 @@ def data_filter_analysis( data ):
     # definindo max e min para o filtro de datas
     min_date = datetime.strptime(str(data['Date'].min().date()), '%Y-%m-%d')
     max_date = datetime.strptime(str(data['Date'].max().date()), '%Y-%m-%d')
-    # slider de datas
+    # slider de datas.
     st.sidebar.header('Filtros Análise')
     st.markdown('#')
     slider_value = st.sidebar.slider(
@@ -361,7 +347,7 @@ if __name__ == "__main__":
 
     simulacao, rendimento, roi = income_simulation(filtered_data, melhor_solucao, capital)
  
-    dataset_previsao = forecast_data( df , 365)
+    dataset_previsao = forecast_data(df, '2023-03-31', 330)
     
     filtered_data_prev = data_filter_forecast(dataset_previsao)
 
